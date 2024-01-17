81fd8e21c5497e9d06b87fa52afa1f382c5f840e
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-01-17 22:51:27 +0100

    Update internet-share.sh

--- scripts/internet-share.sh
+++ scripts/internet-share.sh
@@ -1,32 +1,50 @@
 #!/bin/sh
 
-# Set up iptables rules Output = You Can Share Internet via eth
+# Define interface variable
+ETH_INTERFACE="enp0s26u1u6"
 
-iptables -A FORWARD -i nekoray-tun -o enp0s26u1u6 -j ACCEPT
-iptables -A FORWARD -i enp0s26u1u6 -o nekoray-tun -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -t nat -A POSTROUTING -o nekoray-tun -j MASQUERADE
+# Define iptables rules for forwarding
+FORWARD_RULE_1="-A FORWARD -i nekoray-tun -o $ETH_INTERFACE -j ACCEPT"
+FORWARD_RULE_2="-A FORWARD -i $ETH_INTERFACE -o nekoray-tun -m state --state RELATED,ESTABLISHED -j ACCEPT"
+MASQUERADE_RULE="-t nat -A POSTROUTING -o nekoray-tun -j MASQUERADE"
 
-# This usefull whem you use nekoray Output = You can ping isp gateway & remote servers 
+# Useful for pinging ISP gateway & remote servers
+MANGLE_RULE="sudo iptables -t mangle -A OUTPUT -s 192.168.8.100 -j MARK --set-mark 1"
+IP_RULE="sudo ip rule add fwmark 1 table 200"
+ROUTE_RULE="sudo ip route add default dev nekoray-tun table 200"
 
-sudo iptables -t mangle -A OUTPUT -s 192.168.8.100 -j MARK --set-mark 1
-sudo ip rule add fwmark 1 table 200
-sudo ip route add default dev nekoray-tun table 200
+# Configure network interface
+CONFIGURE_INTERFACE="ip addr add 192.168.1.1/24 dev $ETH_INTERFACE
+                    ip link set $ETH_INTERFACE up"
 
-# Configure network interface 
+# Enable IP forwarding
+ENABLE_IP_FORWARDING="sysctl -w net.ipv6.conf.all.forwarding=1
+                      sysctl -w net.ipv4.ip_forward=1"
 
-ip addr add 192.168.1.1/24 dev enp0s26u1u6
-ip link set enp0s26u1u6 up
+# Xbox Port Forwarding
+XBOX_PORT_FORWARDING="iptables -t nat -A PREROUTING -i nekoray-tun -p udp --dport 3074 -j DNAT --to-destination 192.168.1.2:3074
+                     iptables -t nat -A PREROUTING -i nekoray-tun -p udp --dport 88 -j DNAT --to-destination 192.168.1.2:88
+                     iptables -t nat -A PREROUTING -i nekoray-tun -p udp --dport 53 -j DNAT --to-destination 192.168.1.2:53"
 
-# IP Forwaring Output = Your router gateway can aceess from outside 
+echo "Setting up iptables rules..."
+# Add iptables rules
+sudo iptables $FORWARD_RULE_1
+sudo iptables $FORWARD_RULE_2
+sudo iptables $MASQUERADE_RULE
 
-sysctl -w net.ipv6.conf.all.forwarding=1
-sysctl -w net.ipv4.ip_forward=1
+# Useful for pinging ISP gateway & remote servers
+eval $MANGLE_RULE
+eval $IP_RULE
+eval $ROUTE_RULE
 
-# Xbox Port Forward Output = Nat Type : Open
+# Configure network interface
+eval $CONFIGURE_INTERFACE
 
-iptables -t nat -A PREROUTING -i nekoray-tun -p udp --dport 3074 -j DNAT --to-destination 192.168.1.2:3074
-iptables -t nat -A PREROUTING -i nekoray-tun -p udp --dport 88 -j DNAT --to-destination 192.168.1.2:88
-iptables -t nat -A PREROUTING -i nekoray-tun -p udp --dport 53 -j DNAT --to-destination 192.168.1.2:53
+# Enable IP forwarding
+eval $ENABLE_IP_FORWARDING
+
+# Xbox Port Forwarding
+eval $XBOX_PORT_FORWARDING
 
 echo "Setup complete."
 exit 0;
