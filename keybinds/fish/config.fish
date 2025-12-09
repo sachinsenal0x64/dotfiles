c5ac9ca41df491e81c2b71cd526c41c3ea34c57f
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-12-09 19:09:55 +0800

    Update fish and hypr configurations for improved environment setup and key bindings

--- keybinds/fish/config.fish
+++ keybinds/fish/config.fish
@@ -10,6 +10,10 @@ set -g fish_greeting
 export UV_VENV_CLEAR=1
 export TERM=xterm-256color ssh
 export PATH="$HOME/go/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export DOCKER_HOST=unix:///run/user/1000/podman/podman.sock
+export FLYCTL_INSTALL="/home/pc/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
 alias ls="eza --icons=auto"
 alias icat="kitten icat"
@@ -19,3 +23,8 @@ alias gst="$HOME/.config/scripts/gitreset.sh"
 starship init fish | source
 zoxide init --cmd cd fish | source
 atuin init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
