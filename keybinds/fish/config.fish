4919cd1d2e250b0baf4210938fc6e9c00963f840
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-01-11 20:34:30 +0800

    Add pnpm configuration to Fish shell setup

--- keybinds/fish/config.fish
+++ keybinds/fish/config.fish
@@ -28,3 +28,9 @@ atuin init fish | source
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# pnpm
+set -gx PNPM_HOME "/home/pc/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
